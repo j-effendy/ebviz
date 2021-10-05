5d8882154092ef566ee6db81ea031d3a6064727d
Jimmy Effendy <jeff0001@student.monash.edu.au>
2021-10-05 20:37:37 +1100

    Add two theme_seagull.R and theme_wombat.R

---
+++ R/theme_seagull.R
@@ -0,0 +1,41 @@
+library(ggplot2)
+
+theme_seagull <- function(){
+  theme_minimal() %+replace%
+
+    theme(
+      # grid elements
+      panel.grid.minor = element_blank(),
+      panel.grid.major.x = element_blank(),
+
+      # strip elements
+      strip.text = element_text(face = "bold",
+                                size = rel(1.1),
+                                margin = margin(0,0,0.5,0, unit = "cm")),
+
+      # axis elements
+      axis.title = element_text(face = "bold", size = rel(1.1)),
+      axis.title.x = element_text(margin = margin(0.5,0,0,0, unit = "cm")),
+      axis.title.y = element_text(margin = margin(0,0.5,0,0, unit = "cm"),
+                                  angle = 90),
+      axis.text = element_text(),
+      axis.line.x = element_line(color = "#222222", size = 1),
+      axis.ticks.x = element_line(),
+
+      # legend elements
+      legend.position = "bottom",
+      legend.direction = "horizontal",
+
+      # plot elements
+      plot.title = element_text(size = rel(1.6),
+                                face = "bold",
+                                hjust = 0,
+                                margin = margin(0,0,0.2,0, unit = "cm")),
+      plot.subtitle = element_text(size = rel(1.1),
+                                   margin = margin(0,0,0.5,0, unit = "cm"),
+                                   hjust = 0),
+      plot.caption = element_text(size = rel(0.8),
+                                  margin = margin(1,0,0,0, unit = "cm"),
+                                  hjust = 1)
+    )
+}
